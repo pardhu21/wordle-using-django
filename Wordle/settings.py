24a98137541e58f84e5ae923af822dd160e5e1d0
Pardhu Inti <pardhuganesh21@gmail.com>
2023-02-10 08:09:33 +0530

    revert

--- Wordle/settings.py
+++ Wordle/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'Wordle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'auditor', 'templates', 'auditor')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
